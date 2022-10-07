b93643b066703ecc25f30a792acdb606b6e2c2fa
@bdukhakim <283jei.com.uz>
2022-10-07 15:25:46 +0500

    project

---
+++ main.js
@@ -0,0 +1,37 @@
+var first = prompt('Введите ваше имя');
+
+console.log('Меня зовут ' + first);
+
+var second = prompt('Сколько вам лет?');
+
+console.log('Мне ' + second);
+
+
+var third = prompt('Сколько будет: 10 + 10 =?');
+console.log('Пример 1: 10 + 10 = 20 Ваш ответ ' + third);
+
+var third = prompt('Сколько будет: 9 - 4 =?');
+console.log('Пример 2: 9 - 4 = 5 Ваш ответ ' + third);
+
+var third = prompt('Сколько будет: 11 * 11 =?');
+console.log('Пример 3: 11 * 11 = 121 Ваш ответ ' + third);
+
+var third = prompt('Сколько будет: 100 / 100 =?');
+console.log('Пример 4: 100 / 100 = 1 Ваш ответ ' + third);
+
+var third = prompt('Сколько будет: 10 % 4 =?');
+console.log('Пример 5: 10 % 4 = 2 Ваш ответ ' + third);
+
+
+var fourth = +prompt('Введите первое число');
+
+var fifth = +prompt('Введите второе число');
+
+var sixth = +prompt('Введите третье число');
+
+
+alert('Среднее арифметическое чисел: ' + (fourth + fifth + sixth) / 3)
+
+
+
+
